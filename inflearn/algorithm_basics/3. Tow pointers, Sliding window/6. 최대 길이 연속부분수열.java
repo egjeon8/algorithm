7f8b9e210d0f8751427187a1737a34c4aa1cdc46
egjeon8 <egjeon8@gmail.com>
2023-01-15 16:33:13 +0900

    최대 길이 연속부분수열(복합적 문제)

---
+++ inflearn/algorithm_basics/3. Tow pointers, Sliding window/6. 최대 길이 연속부분수열.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0, cnt = 0, lt = 0;
+        for (int rt = 0; rt < n; rt++)
+        {
+            if (arr[rt] == 0) cnt++;
+            while (cnt > m)
+            {
+                if (arr[lt++] == 0) cnt--;
+            }
+            answer = Math.max(answer, rt - lt + 1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int m = sc.nextInt();
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        System.out.println(T.solution(n, m, arr));
+    }
+}
