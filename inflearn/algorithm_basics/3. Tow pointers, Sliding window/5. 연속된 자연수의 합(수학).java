8891bb5f8ce1943063ecaacd99be400a339c2557
egjeon8 <egjeon8@gmail.com>
2023-01-15 14:19:15 +0900

    연속된 자연수의 합(수학)

---
+++ inflearn/algorithm_basics/3. Tow pointers, Sliding window/5. 연속된 자연수의 합(수학).java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution(int n) {
+        int answer = 0, cnt = 1;
+        n--;
+        while (n > 0)
+        {
+            cnt++;
+            n -= cnt;
+            if (n % cnt == 0) answer++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(T.solution(n));
+    }
+}
